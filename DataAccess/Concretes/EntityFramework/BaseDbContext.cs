54fd4053d3372cb3ba949322f63a3238647ae124
Fatma Birel <fatmabrl11@gmail.com>
2024-05-30 14:52:21 +0300

    Register & Login Operations

--- DataAccess/Concretes/EntityFramework/BaseDbContext.cs
+++ DataAccess/Concretes/EntityFramework/BaseDbContext.cs
@@ -9,6 +9,7 @@ namespace DataAccess.Concretes.EntityFramework
     {
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=DESKTOP-Q270QVE\\SQLEXPRESS; Database=Tobeto4A; Trusted_Connection=True;TrustServerCertificate=True");
